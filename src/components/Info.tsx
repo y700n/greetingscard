c44c27201a45ae11ead2043f9b360fcf01101b16
y700n <seung.yoon.yu@gmail.com>
2026-09-13 20:29:56 +0900

    fix: 피드백 보내기 기능 삭제

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,12 +1,16 @@
-import type { SetStateAction } from 'preact/compat'
-import { useMemo, type Dispatch } from 'preact/hooks'
-import desc from '../assets/docs/desc.txt?raw'
-import img from '../assets/logo.svg'
+import type { SetStateAction } from "preact/compat";
+import { useMemo, type Dispatch } from "preact/hooks";
+import desc from "../assets/docs/desc.txt?raw";
+import img from "../assets/logo.svg";
 
-export function Info({ setState }: { setState: Dispatch<SetStateAction<boolean>> }) {
-  const txt = useMemo(() => desc.split('\n'), [])
+export function Info({
+  setState,
+}: {
+  setState: Dispatch<SetStateAction<boolean>>;
+}) {
+  const txt = useMemo(() => desc.split("\n"), []);
   return (
-    <div class="info" onClick={() => setState(s => !s)}>
+    <div class="info" onClick={() => setState((s) => !s)}>
       <h1>격식 있는 연하장 생성기</h1>
       <>
         {txt.map((t, i) => (
@@ -16,14 +20,6 @@ export function Info({ setState }: { setState: Dispatch<SetStateAction<boolean>>
         ))}
       </>
       <img src={img} alt="hoi logo" class="logo" />
-      <a
-        href="https://docs.google.com/forms/d/e/1FAIpQLSfJPUrMG4ifxN7xE6qvyAscrzU3dA95GdnT0ulmkp_fO2qWxA/viewform?usp=sharing&ouid=115863162269024975697"
-        target="_blank"
-        rel="noopener noreferrer"
-        class="kakao-overlay-btn"
-        onClick={e => e.stopPropagation()}>
-        피드백 보내기
-      </a>
     </div>
-  )
+  );
 }
